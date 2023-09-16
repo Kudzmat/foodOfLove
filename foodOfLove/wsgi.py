00672434f754ea8691463774a91064d48d01dcde
Kudz <kudzayibamhare@gmail.com>
2023-09-16 16:11:00 +0200

    Initial commit

---
+++ foodOfLove/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for foodOfLove project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodOfLove.settings")
+
+application = get_wsgi_application()
