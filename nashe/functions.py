46f5d86d182acab49d2659bd9dc80fdc9e8bcf56
Kudz <kudzayibamhare@gmail.com>
2023-09-21 13:31:40 +0200

    finshed code to display word cloud and banners for all 3 Tinashe albums

---
+++ nashe/functions.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+TINASHE_DATA = pd.read_csv('data/tinashe.csv')
+
+
+# Get album lyrics from user input
+def get_album_lyrics(album):
+    album_lyrics = []  # empty list will hold all the lyrics from an album by track
+
+    for index, row in TINASHE_DATA.iterrows():
+        if row['album'] == album:
+            lyrics = row['lyrics']
+            album_lyrics.append(lyrics)
+
+    return album_lyrics
+
+
+def get_project_lyrics():
+    proj_lyrics = []  # empty list will hold all the lyrics from an album by track
+
+    for index, row in TINASHE_DATA.iterrows():
+        lyrics = row['lyrics']
+        proj_lyrics.append(lyrics)
+
+    return proj_lyrics
+
+
+# Processing the lyrics by cleaning the up
+def process_lyrics(lyrics_list):
+    processed = []
+    custom_stopwords = ['yeah', 'oh', 'nigga', 'chorus', 'Drake', 'drake', 'niggas']
+    for lyrics in lyrics_list:
+        new_lyrics = lyrics.lower()
+        tokens = word_tokenize(new_lyrics)
+        # Remove punctuation and stopwords (both English and custom)
+        tokens = [word for word in tokens if word.isalpha() and word.lower() not in stopwords.words(
+            'english') and word.lower() not in custom_stopwords]
+        processed.extend(tokens)  # Extend the list with the tokens
+    return processed
+
+
+def check_frequency(tokens):
+    lyrics_count = {}  # This dictionary will hold a word and the number of times it appears
+    word_frequency = Counter(tokens)
+    # Find the 10 most common words
+    most_common_words = dict(word_frequency.most_common(20))
+
+    # Print the most common words and their frequencies
+    for word, num in most_common_words.items():
+        lyrics_count[word] = num
+
+    return lyrics_count
+
+
+def create_bar_graph(count_dict):
+    words = list(count_dict.keys())
+    appearances = list(count_dict.values())
+    plt.figure(figsize=(15, 10))
+    plt.bar(range(len(count_dict)), appearances, tick_label=words)
+    plt.show()
+
+
+def create_word_cloud(words):
+    # Combine the words into a single string
+    text = " ".join([word for word in words.keys()])
+
+    # Generate the word cloud
+    word_cloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate_from_frequencies(words)
+
+    return word_cloud
--- nashe/views.py
+++ nashe/views.py
@@ -1,6 +1,103 @@
 from django.shortcuts import render
+from io import BytesIO
+import base64
+import os
+from .functions import *
+from urllib.parse import unquote
 
 
 # Create your views here.
 def nashe_albums(request):
     return render(request, "nashe/explore_tinashe.html")
+
+
+def get_projects_cloud(request):
+    banner_path = os.path.join('static', 'images', 'albums', 'nashe', '7.png')
+
+    with open(banner_path, 'rb') as image_file:
+        banner = base64.b64encode(image_file.read()).decode("utf-8")
+
+    album = 'All Projects'
+
+    # get selected album's lyrics
+    proj_Lyrics = get_project_lyrics()
+    print(proj_Lyrics)
+
+    # clean up lyrics
+    clean_lyrics = process_lyrics(proj_Lyrics)
+
+    # get word count
+    word_count = check_frequency(clean_lyrics)
+
+    # create word cloud
+    word_cloud = create_word_cloud(word_count)
+
+    # Convert the word cloud image to bytes
+    image_stream = BytesIO()
+    word_cloud.to_image().save(image_stream, format="PNG")  # Save as PNG format (or the desired format)
+    image_stream.seek(0)
+    encoded_image = base64.b64encode(image_stream.read()).decode("utf-8")
+
+    context = {
+        'word_cloud': encoded_image,
+        'banner': banner,
+        'album': album
+    }
+
+    return render(request, 'nashe/word_cloud.html', context=context)
+
+
+# getting word cloud for singular album
+def get_album_cloud(request, encoded_album):
+    # Decode the album name from the URL
+    album = unquote(encoded_album)
+
+    if album == "Songs For You":
+        banner_path = os.path.join('static', 'images', 'albums', 'nashe', '10.png')
+
+        with open(banner_path, 'rb') as image_file:
+            banner = base64.b64encode(image_file.read()).decode("utf-8")
+
+    elif album == '333':
+        banner_path = os.path.join('static', 'images', 'albums', 'nashe', '9.png')
+
+        with open(banner_path, 'rb') as image_file:
+            banner = base64.b64encode(image_file.read()).decode("utf-8")
+
+    elif album == 'BBANG3L':
+        banner_path = os.path.join('static', 'images', 'albums', 'nashe', '8.png')
+
+        with open(banner_path, 'rb') as image_file:
+            banner = base64.b64encode(image_file.read()).decode("utf-8")
+
+    else:
+        banner_path = os.path.join('static', 'images', 'albums', 'nashe', '7.png')
+
+        with open(banner_path, 'rb') as image_file:
+            banner = base64.b64encode(image_file.read()).decode("utf-8")
+
+    # get selected album's lyrics
+    album_Lyrics = get_album_lyrics(album)
+
+    # clean up lyrics
+    clean_lyrics = process_lyrics(album_Lyrics)
+
+    # get word count
+    word_count = check_frequency(clean_lyrics)
+
+    # create word cloud
+    word_cloud = create_word_cloud(word_count)
+
+    # Convert the word cloud image to bytes
+    image_stream = BytesIO()
+    word_cloud.to_image().save(image_stream, format="PNG")  # Save as PNG format (or the desired format)
+    image_stream.seek(0)
+    encoded_image = base64.b64encode(image_stream.read()).decode("utf-8")
+
+    context = {
+        'word_cloud': encoded_image,
+        'album': album,
+        'banner': banner
+    }
+
+    return render(request, 'nashe/word_cloud.html', context=context)
